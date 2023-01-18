56ef219f309ff490dd2e9df2ea771c2431d0f7a1
Samet Dundar <samet.dundar@applogist.com>
2023-01-18 10:18:23 +0300

    flag

--- app/src/main/java/com/applogist/applogistpaginglibrary/adapter/PopularMovieViewHolder.kt
+++ app/src/main/java/com/applogist/applogistpaginglibrary/adapter/PopularMovieViewHolder.kt
@@ -4,6 +4,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.applogist.applogistpaginglibrary.data.response.Item
 import com.applogist.applogistpaginglibrary.databinding.ItemLayoutBinding
 import com.applogist.applogistpaginglibrary.utils.executeWithAction
+import com.applogist.applogistpaginglibrary.utils.loadImage
 
 class PopularMovieViewHolder(private val binding: ItemLayoutBinding, private val itemClick: (Item) -> Unit) :
     RecyclerView.ViewHolder(binding.root) {
@@ -11,6 +12,7 @@ class PopularMovieViewHolder(private val binding: ItemLayoutBinding, private val
         binding.executeWithAction {
             this.item = result
         }
+        binding.popularMovieImageView.loadImage(result.imageUrl)
 //        binding.textViewRelease.text = "(" +result.release_date?.split("-")?.get(0) + ")"
 //        var date = result.release_date?.split("-")
 //        binding.releaseDate.text = date?.get(2) + "." + date?.get(1) + "." + date?.get(0)
